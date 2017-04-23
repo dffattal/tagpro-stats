07d7f575ba1e49438525c3f83621173dc62b9bc7
Daniel Fattal <dffattal@gmail.com>
2017-04-22 20:19:33 -0400

    decrease seeding speed

--- oldsiteaccounts.js
+++ oldsiteaccounts.js
@@ -5,15 +5,17 @@ const fetchStats = require('./server/schedule')
 for (let i = 1; i < 117922; i++) {
   setTimeout(function() {
     console.log('Currently seeding account #', i)
+    console.log('Attempting to get tagpro-stats profile page...')
     axios.get(`http://tagpro-stats.com/profile.php?userid=${i}`)
       .then(response => response.data)
       .then(data => {
         const $ = cheerio.load(data)
         if (+$('.stat')[3].children[0].data > 300) {
           const url = $('h3')[0].children[1].attribs.href
+          console.log('Attempting to add account to database...')
           return axios.post('https://tagpro-stats.herokuapp.com/api/accounts/', {url})
         }
       })
       .catch(console.error)
-  }, i * 100)
+  }, i * 1000)
 }
