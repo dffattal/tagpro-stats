fa7dc52fc2843e99a8a4e60d76004b743a83b29b
Daniel Fattal <dffattal@gmail.com>
2017-04-24 22:44:48 -0400

    seeding test

--- oldsiteaccounts.js
+++ oldsiteaccounts.js
@@ -2,20 +2,18 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fetchStats = require('./server/schedule')
 
-for (let i = 24500; i < 117922; i++) {
-  setTimeout(function() {
-    console.log('Currently seeding account #', i)
-    console.log('Attempting to get tagpro-stats profile page...')
-    axios.get(`http://tagpro-stats.com/profile.php?userid=${i}`)
-      .then(response => response.data)
-      .then(data => {
-        const $ = cheerio.load(data)
-        if (+$('.stat')[3].children[0].data > 300) {
-          const url = $('h3')[0].children[1].attribs.href
-          console.log('Attempting to add account to database...')
-          return axios.post('https://tagpro-stats.herokuapp.com/api/accounts/', {url})
-        }
-      })
-      .catch(console.error)
-  }, i * 1000 - 24500 * 1000)
+for (let i = 24500; i < 25500; i++) {
+  console.log('Currently seeding account #', i)
+  console.log('Attempting to get tagpro-stats profile page...')
+  axios.get(`http://tagpro-stats.com/profile.php?userid=${i}`)
+    .then(response => response.data)
+    .then(data => {
+      const $ = cheerio.load(data)
+      if (+$('.stat')[3].children[0].data > 300) {
+        const url = $('h3')[0].children[1].attribs.href
+        console.log('Attempting to add account to database...')
+        return axios.post('https://tagpro-stats.herokuapp.com/api/accounts/', {url})
+      }
+    })
+    .catch(console.error)
 }
